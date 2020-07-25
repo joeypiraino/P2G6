a929568d27729118705c727ea267b615da0f861a
Big Sic <joeypiraino@gmail.com>
2020-07-25 13:46:51 -0500

    new hbs files and routing

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+/** @format */
+
 var express = require("express");
 
 var PORT = process.env.PORT || 63715;
@@ -18,12 +20,12 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/maintenance_controller.js");
+var routes = require("./routes");
 
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+app.listen(PORT, function () {
+	// Log (server-side) when our server has started
+	console.log("Server listening on: http://localhost:" + PORT);
 });
